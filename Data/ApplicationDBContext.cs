9eb1f2430d8f47f20d8bfb59c20b3f3f1b2311f7
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2024-09-22 13:02:55 +0300

    setup db

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Data
+{
+    // This class will be used to interact with the database, allowing us
+    // to perform CRUD operations on the database. Basically allows us to
+    // choose what table we want.
+    public class ApplicationDBContext : DbContext
+    {
+        // The `base` is for passing the options to the base class (DbContext)
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+// DbSet allows us to manipulate the database table (create tables, etc).
+        public DbSet<Models.Stock> Stocks { get; set; } = null!;
+        public DbSet<Models.Comment> Comments { get; set; } = null!;
+    }
+}
